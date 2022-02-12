68c881381d546296622ac9e67d4915ae1098e73e
KwiecienO <95612576+KwiecienO@users.noreply.github.com>
2022-02-12 15:55:17 +0100

    Feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,42 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+     const chunks = new Array();
+    const numChunks = Math.ceil(expr.length / 10);
+
+    for (let i = 0, o = 0; i < numChunks; ++i, o += 10) {
+      chunks[i] = expr.substr(o, 10)
+    }
+    for (let j=0; j < numChunks; j++) {
+        let str=chunks[j]; 
+        let strArr = str.split("");
+    while(strArr[0] === "0") strArr.shift();
+    let arrString= strArr.join('')
+    chunks[j] =arrString;
+    }
+
+
+
+
+
+for (let z =0; z < numChunks; z++ ) {
+chunks[z] = chunks[z]
+    .replace(/11/g, '-')
+    .replace(/10/g, '.');
+}
+
+  let result = [];
+  for (let z =0; z < numChunks; z++ ) {
+  result[z] = MORSE_TABLE[chunks[z]];
+  }
+  console.log(result.join(''));
+  return result.join('');
+
+
 }
 
 module.exports = {
